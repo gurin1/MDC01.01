d33522d441b17cddb8b02a170de867ae3862098f
Хвастунов <op.ap.191@mail.ru>
2022-12-05 09:50:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 5;
+
+int[] a = new[] { 2, 8, 3, 6, 9, 4, 5 };
+
+int count = 0;
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] > x)
+    {
+        sum += a[i];
+        count++;
+    }
+}
+
+Console.WriteLine(sum / count);
+
